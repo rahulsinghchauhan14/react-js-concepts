429bf33a1544a3bcaee8dbbf95b0f9b51c80ccd6
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2023-11-07 23:18:06 +0530

    Usage of context api

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react' 
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>React Context API</h1>
+      <Login></Login>
+      <Profile></Profile>
+    </UserContextProvider>
+  )
+}
+
+export default App
